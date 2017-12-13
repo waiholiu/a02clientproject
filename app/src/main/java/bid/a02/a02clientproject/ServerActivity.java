724ba09e313edf22368eddc2df9c42df49f2c5f8
waiholiu <waiholiu>
2017-12-13 22:32:49 +1100

    connected to server to get token

--- app/src/main/java/bid/a02/a02clientproject/ServerActivity.java
+++ app/src/main/java/bid/a02/a02clientproject/ServerActivity.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ServerActivity extends AppCompatActivity {
@@ -26,23 +32,67 @@ public class ServerActivity extends AppCompatActivity {
         mTextView = (TextView) findViewById(R.id.textView);
 
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://testbasicauth20170222085409.azurewebsites.net/api/values";
+        String url = "https://testbasicauth20170222085409.azurewebsites.net/token";
+//        String url2 = "https://testbasicauth20170222085409.azurewebsites.net/api/values";
+//
+//// Request a string response from the provided URL.
+//        StringRequest stringRequest = new StringRequest(Request.Method.GET, url2,
+//                new com.android.volley.Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//                        // Display the first 500 characters of the response string.
+//                        mTextView.setText("Response is: " + response.toString());
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                mTextView.setText("error " + error.getMessage());
+//            }
+//
+//
+//        });
+//// Add the request to the RequestQueue.
+//        queue.add(stringRequest);
 
-// Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new com.android.volley.Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        mTextView.setText("Response is: " + response.toString());
-                    }
-                }, new Response.ErrorListener() {
+
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("grant_type", "password");
+            jsonBody.put("username", "2@test.com");
+            jsonBody.put("password", "Sample1!");
+
+        }
+        catch(Exception ex)
+        {}
+
+        queue.add(new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                mTextView.setText("onResponse = \n " + response.toString());
+            }
+        }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                mTextView.setText("error " + error.getMessage());
+                mTextView.setText("response error \n" + error.networkResponse.statusCode);
             }
+        }) {
+
+            @Override
+            public String getBodyContentType() {
+                return "application/x-www-form-urlencoded; charset=UTF-8";
+            }
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("username", "2@test.com");
+                params.put("password", "Sample1!");
+                params.put("grant_type", "password");
+                return params;
+            }
+
         });
-// Add the request to the RequestQueue.
-        queue.add(stringRequest);
+
+
     }
 }
