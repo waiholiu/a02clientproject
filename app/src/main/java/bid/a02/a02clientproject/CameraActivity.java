0e7b7470a56ee5963023f7de84f93412dc9b8876
waiholiu <waiholiu>
2018-02-04 18:32:56 +1100

    got a photo taken and saved to disc

--- app/src/main/java/bid/a02/a02clientproject/CameraActivity.java
+++ app/src/main/java/bid/a02/a02clientproject/CameraActivity.java
@@ -2,15 +2,24 @@ package bid.a02.a02clientproject;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.GridLayout;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CameraActivity extends AppCompatActivity {
 
@@ -39,24 +48,77 @@ public class CameraActivity extends AppCompatActivity {
 
     public void btnCamera(View view) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "bid.a02.a02clientproject.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+
+
+//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//
+//            GridLayout grid = (GridLayout)findViewById(R.id.gridPhotos);
 
-            GridLayout grid = (GridLayout)findViewById(R.id.gridPhotos);
+//            ImageView newImageView = new ImageView(this);
+//            newImageView.setImageBitmap(imageBitmap);
+//            grid.addView(newImageView);
 
-            ImageView newImageView = new ImageView(this);
-            newImageView.setImageBitmap(imageBitmap);
-            grid.addView(newImageView);
+            File imgFile = new  File(mCurrentPhotoPath);
+
+            if(imgFile.exists()){
+
+                GridLayout grid = (GridLayout)findViewById(R.id.gridPhotos);
+
+                Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+
+                ImageView myImage = new ImageView(this);
+
+                myImage.setImageBitmap(myBitmap);
+                grid.addView(myImage);
+
+            }
+
+
 
         }
+    }
+
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
 
